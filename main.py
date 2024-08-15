e81d38b2c95850b65d6e655932355ffe562938af
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-08-15 18:41:28 +0200

    add root

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import customtkinter as CTk
 
 def main():
     print("Hello World!")
-    root = CTk.ctk()
+    root = CTk.CTk()
+    root.geometry("450x500+2000+300")
+    root.title("WHC - Working hours calculator")
     root.mainloop()
 
 if __name__ == '__main__':
