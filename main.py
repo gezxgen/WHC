27c0688c4d85c0294c40146b94dd22bc90aafa18
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-08-15 19:33:57 +0200

    add Frame

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import customtkinter as CTk
+import customtkinter as ctk
+
+
+class Root(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("450x500+2000+300")
+        self.title("WHC - Working hours calculator")
+
+        self.input = Input(master=self, width=250, height=425)
+        self.input.pack(side="top", anchor="w", padx=10, pady=10)
+
+
+class Input(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
 
 def main():
-    print("Hello World!")
-    root = CTk.CTk()
-    root.geometry("450x500+2000+300")
-    root.title("WHC - Working hours calculator")
-    input = CTk.CTkFrame(master=root, width=250, height=425)
-    input.pack(side="top", anchor="w", padx=10, pady=10)
+    root = Root()
     root.mainloop()
 
 if __name__ == '__main__':
